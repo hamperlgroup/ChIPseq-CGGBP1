533a5f9a96dc9b477ab02b78c9d3c6b090d70c0a
Elizabeth-mqz-gmz <elimqzgmzhp@comunidad.unam.mx>
2024-09-17 11:37:54 +0200

    Update fixing igv tracks

--- workflow/scripts/gviz_tracks.R
+++ workflow/scripts/gviz_tracks.R
@@ -132,6 +132,7 @@ library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 library(org.Hs.eg.db)
 library(AnnotationDbi)
 library(biomaRt)
+library(Homo.sapiens)
 
 ## Outdir
 outDir <- "/lustre/groups/ies/projects/hamperl_lab/elizabeth.marquezgom/Augusto/ChIPseq-CGGBP1/results/genome_tracks"
@@ -149,17 +150,29 @@ bg_ctrl_track <- DataTrack(range = bg_ctrl, type = "hist", genome = "hg38", name
 PlotTrack <- function(gene) {
     # Select the gene of interest
     gene_symbol <- gene
-    gene_info <- select(org.Hs.eg.db, keys = gene_symbol, columns = c("SYMBOL", "ENTREZID"), keytype = "SYMBOL")
-    entrez_id <- gene_info$ENTREZID
-    gene_range <- genes(txdb, filter = list(gene_id = entrez_id))
+    gene_info <- select(Homo.sapiens,
+        keys = gene_symbol,
+        columns = c("GENEID", "GENENAME"), keytype = "SYMBOL"
+    )
+    gene_tx <- transcripts(Homo.sapiens,
+        filter = list("gene_id" = gene_info$GENEID),
+        columns = c("GENEID", "TXNAME", "SYMBOL")
+    )
+    names(mcols(gene_tx)) <- c("gene", "transcript", "symbol")
+    # entrez_id <- gene_info$ENTREZID
+    # gene_range <- genes(txdb, filter = list(gene_id = entrez_id))
 
     # Create the gene region track
-    gene_track <- GeneRegionTrack(txdb,
-        chromosome = as.character(seqnames(gene_range)),
-        start = start(gene_range) - 1000000, end = end(gene_range) + 1000000,
-        geneSymbol = FALSE, name = "Gene region", strand = strand(gene_range), collapseTranscripts = TRUE,
-        transcriptAnnotation = FALSE
+    gene_track <- GeneRegionTrack(gene_tx,
+        genome = "hg38",
+        chromosome = as.character(seqnames(gene_tx)),
+        start = start(gene_tx) - 1000000, end = end(gene_tx) + 1000000,
+        showId = TRUE, collapseTranscripts = TRUE
     )
+    # start = start(gene_range) - 1000000, end = end(gene_range) + 1000000,
+    # geneSymbol = FALSE, name = "Gene region", strand = strand(gene_range), collapseTranscripts = TRUE,
+    # transcriptAnnotation = FALSE
+    # )
 
     # transcript_ids <- symbol(gene_track)
     # # Use biomaRt to get gene symbols corresponding to ENST IDs
@@ -178,15 +191,14 @@ PlotTrack <- function(gene) {
     # gene_symbols <- mapIds(org.Hs.eg.db, keys = transcript_ids, column = "SYMBOL", keytype = "ENSEMBLTRANS", multiVals = "first")
 
     # Load chromosome
-    ideoTrack <- IdeogramTrack(genome = "hg38", chromosome = gene_track@chromosome, from = from, to = to)
+    ideoTrack <- IdeogramTrack(genome = "hg38", chromosome = gene_track@chromosome)
     axTrack <- GenomeAxisTrack()
 
     # Plot the tracks
     pdf(paste0(outDir, "/", gene, "_track.pdf"))
     plotTracks(list(ideoTrack, axTrack, gene_track, bg_encode_track, bg_ctrl_track),
-        from = start(gene_range) - 1000000, to = end(gene_range) + 1000000,
-        chromosome = as.character(seqnames(gene_range)), transcriptAnnotation = FALSE, showBandId = TRUE, cex.bands = 0.9,
-        collapseTranscripts = TRUE
+        from = gene_track@start - 1000000, to = gene_track@end + 1000000,
+        chromosome = gene_track@chromosome, showBandId = TRUE, cex.bands = 0.9
     )
     dev.off()
 }
@@ -195,3 +207,55 @@ genes <- c("IRF2BPL", "RPS29", "C7orf50", "EGR1", "PBX1", "EIF3F", "SEC22B")
 genes <- c("IRF2BPL")
 
 lapply(genes, PlotTrack)
+
+###########################################
+##############
+library(Gviz)
+library(rtracklayer)
+library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+library(org.Hs.eg.db)
+library(AnnotationDbi)
+library(biomaRt)
+library(Homo.sapiens)
+
+## Outdir
+outDir <- "/lustre/groups/ies/projects/hamperl_lab/elizabeth.marquezgom/Augusto/ChIPseq-CGGBP1/results/genome_tracks"
+
+# Load the BigWig file
+bg_encode <- "/lustre/groups/ies/projects/hamperl_lab/elizabeth.marquezgom/Augusto/ChIPseq-CGGBP1/results/covmean/ChIP-seq_CGGBP1_ENCODE.coverage.bedgraph"
+bg_encode_track <- DataTrack(range = bg_encode, type = "hist", genome = "hg38", name = "ChIP-seq CGGBP1", col.histogram = "darkblue", fill.histogram = "darkblue", ylim = c(0, 1))
+
+bg_ctrl <- "/lustre/groups/ies/projects/hamperl_lab/elizabeth.marquezgom/Augusto/ChIPseq-CGGBP1/results/covmean/ChIP-seq_ctrl.coverage.bedgraph"
+bg_ctrl_track <- DataTrack(range = bg_ctrl, type = "hist", genome = "hg38", name = "ChIP-seq CGGBP1 control", col.histogram = "darkblue", fill.histogram = "darkblue", ylim = c(0, 1))
+
+
+# Select the gene of interest
+gene_info <- select(Homo.sapiens,
+    keys = c("RPS29", "RN7SL1"),
+    columns = c("GENEID", "GENENAME"), keytype = "SYMBOL"
+)
+gene_tx <- transcripts(Homo.sapiens,
+    filter = list("gene_id" = gene_info$GENEID),
+    columns = c("GENEID", "TXNAME", "SYMBOL")
+)
+names(mcols(gene_tx)) <- c("gene", "transcript", "symbol")
+
+# Create the gene region track
+gene_track <- GeneRegionTrack(gene_tx,
+    genome = "hg38",
+    chromosome = as.character(seqnames(gene_tx)),
+    start = start(gene_tx) - 500000, end = end(gene_tx) + 500000,
+    showId = TRUE, collapseTranscripts = FALSE
+)
+
+# Load chromosome
+ideoTrack <- IdeogramTrack(genome = "hg38", chromosome = gene_track@chromosome)
+axTrack <- GenomeAxisTrack()
+
+# Plot the tracks
+pdf(paste0(outDir, "/", gene, "_track.pdf"))
+plotTracks(list(ideoTrack, axTrack, gene_track, bg_encode_track, bg_ctrl_track),
+    from = gene_track@start - 500000, to = gene_track@end + 500000,
+    chromosome = gene_track@chromosome, showBandId = TRUE, cex.bands = 0.9
+)
+dev.off()
